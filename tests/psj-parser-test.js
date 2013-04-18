7b7a1fc3cdb0cf9007bdffd6c7e6c64d6475a114
Justin Spears <speajus@gmail.com>
2013-04-18 17:44:38 -0400

    *fixed parser to work with multiple nested tags.

--- tests/psj-parser-test.js
+++ tests/psj-parser-test.js
@@ -11,78 +11,14 @@ var xmltests = {
     '<c:out var="test">hello<b/></c:out>':'prefix:c, tag:out attrs: {\"var\":\"test\"}, buffer:hello<b/>',
     '<c:set var="#"/>':"prefix:c, tag:set attrs: {\"var\":\"#\"}",
     '<c:set var="#">hello</c:set>':"prefix:c, tag:set attrs: {\"var\":\"#\"}, buffer:hello",
-    '<!-- hello <c:set var="#"/> me -->':"content:<!-- hello  prefix:c, tag:set attrs: {\"var\":\"#\"} content: me -->"
+    '<!-- hello <c:set var="#"/> me -->':"hello <c:set var=\"#\"/> me",
+    '<c:choose><c:when test="abc">hello</c:when><c:otherwise>goodbye</c:otherwise></c:choose>':"prefix:c, tag:choose attrs: {}, buffer:<c:when test=\"abc\">hello</c:when><c:otherwise>goodbye</c:otherwise>",
+    '<c:w t="abc">hell</c:w><c:o>h7n</c:o>':"prefix:c, tag:w attrs: {\"t\":\"abc\"}, buffer:hell prefix:c, tag:o attrs: {}, buffer:h7n",
+    '<%@taglib prefix="core"  uri="http://java.sun.com/jsp/jstl/core" %>\n<core:choose><core:when test="${empty myitems}">hello</core:when><core:otherwise>goodbye</core:otherwise></core:choose>':"prefix:core, tag:choose attrs: {}, buffer:<core:when test=\"${empty myitems}\">hello</core:when><core:otherwise>goodbye</core:otherwise>",
+    '<%@taglib prefix="core"  uri="http://java.sun.com/jsp/jstl/core" %>\n<core:when test=\"${empty myitems}\">hello</core:when><core:otherwise>goodbye</core:otherwise>':"prefix:core, tag:when attrs: {\"test\":\"${empty myitems}\"}, buffer:hello prefix:core, tag:otherwise attrs: {}, buffer:goodbye"
+
 };
 var tests ={
-    'parse some body':function(test){
-        parser = new Parser();
-        console.log(printResult(parser.parseBody('<%-- comment --%> hello ${var} with ${stuff} <c:out>more stuff</c:out>').context));
-
-        test.done();
-    },
-    'throw exeption unknown page directive':function(test){
-        parser = new Parser();
-        var threw = true;
-        try {
-        var result = parser.parse('<%-- hello --%><%@ taglib prefix="p" tagdir="."%>  <%@nosuchtag %>')
-            threw = false;
-        }catch(e){
-            test.equal(e.message, "Can not have tag directives in the body @[0,5] -%><%--\"@\"--  taglib");
-        }
-        test.equals(threw, true, "throw an error")
-        test.done();
-    },
-    'test if taglib parsed': function (test) {
-        parser = new Parser();
-        var result = parser.parse('<%@ taglib prefix="p" tagdir="."%>').context;
-        test.equal(result.taglib.p.prefix, 'p');
-        test.equal(result.taglib.p.tagdir, '.');
-        test.done();
-
-    },
-
-    'parsing attributes parsed': function (test) {
-        parser = new Parser();
-        var result = parser.parse('<%@ attribute name="people" type="Array"%>').context;
-        test.equal(result.attribute[0].name, 'people');
-        test.equal(result.attribute[0].type, 'Array');
-        test.done();
-
-    },
-    'do comments work': function (test) {
-        parser = new Parser();
-        var result = parser.parse('<%-- hello\n this is a comment --%>').context;
-
-        test.equal(result.content[0](), ' hello\n this is a comment ');
-        test.done();
-    },
-    'do comments work not ended': function (test) {
-        parser = new Parser();
-        var e = null;
-        try {
-           parser.parse('<%-- hello\n this is a comment %>');
-        } catch (err) {
-            e = err;
-        }
-        test.ok(e != null);
-        test.done();
-    },
-    'parsing of ${} eval statements': function (test) {
-        parser = new Parser();
-        var result = parser.parse('stuff ${isneat} really');
-        var evl = result.content[1]()
-        console.log('result', evl);
-        test.equal('isneat', evl);
-        test.done();
-
-    },
-    'parsing a jsp tag file ': function (test) {
-        parser = new Parser();
-        var str = fs.readFileSync('example/tags/people.tag', 'utf-8');
-        var result = parser.parse(str);
-        console.log('result', result);
-        test.done();
-    }
 };
 
 Object.keys(xmltests).forEach(function makeTestFunction(k){
@@ -92,7 +28,7 @@ Object.keys(xmltests).forEach(function makeTestFunction(k){
 var core = '<%@taglib prefix="c"  uri="http://java.sun.com/jsp/jstl/core" %>'
 function printResult(result) {
     return result && result.content.map(function (v) {
-        return v()
+        return typeof v === 'function' ? v() :v;
     }).join(' ');
 }
 function makeTest(xml, expected){
